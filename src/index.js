e46fff8e4d60739db8cf66b305cda675ee794447
Nadya Kanash <nadida.hi@gmail.com>
2020-09-29 11:14:57 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length === 0) return 0;
+    return Math.min.apply(Math, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0) return 0;
+    return Math.max.apply(Math, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0) return 0;
+    let result = array.reduce(function(sum, item) {
+        return sum + item;
+    }, 0);
+    return result/array.length;
 }
